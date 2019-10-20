dea456720025cc82772d21cbe5e319e1788466ca
calvinvuong <calvinvuong425@gmail.com>
2019-10-20 19:26:26 -0400

    Created thread for neighbor connection socket.

--- p2p.java
+++ p2p.java
@@ -6,23 +6,44 @@ import java.util.Scanner;
 
 public class p2p {
 
-    int neighborPort; // port num for neighbor connections
-    int transferPort; // port num for transfers
-
+    static int neighborPort; // port num for neighbor connections
+    static int transferPort; // port num for transfers
+    static ServerSocket welcomeNeighborSocket;
+    static ServerSocket welcomeTransferSocket;
+    
     public static void main(String[] args) {
-	p2p peer = new p2p();
-
+	readPorts(); // read port nums from config file
+	// create welcome sockets
+	try {
+	    welcomeNeighborSocket = new ServerSocket(neighborPort);
+	    welcomeTransferSocket = new ServerSocket(transferPort);
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	}
+	
+	while (true) {
+	    try {
+		Socket neighborSocket = welcomeNeighborSocket.accept();
+		System.out.println("New connection.");
+		Thread t = new Thread( new NeighborThread(neighborSocket) );
+		
+		t.start();
+	    }
+	    catch (Exception e) {
+		e.printStackTrace();
+	    }
+	}
+	
+	/*
+	  System.out.println(neighborPort);
+	  System.out.println(transferPort);
+	*/
     }
     
-    // Constructor
-    public p2p() {
-	readPorts(); // read port nums from config file
-	System.out.println(neighborPort);
-	System.out.println(transferPort);
-    }
 
     // Sets the port numbers for this peer based on config file.
-    public void readPorts() {
+    public static void readPorts() {
 	try {
 	    File peerConfig = new File("config_peer.txt");
 	    Scanner scan = new Scanner(peerConfig);
@@ -33,8 +54,28 @@ public class p2p {
 	    e.printStackTrace();
 	}
     }
+}
 
+// Handles operations relating to the socket between connected ppers
+class NeighborThread implements Runnable {
+    Socket connectionSocket;
+    BufferedReader in; // in from neighbor
+    DataOutputStream out; // out to neighbor
+    
+    public NeighborThread(Socket connection) {
+	connectionSocket = connection;
+	try {
+	    in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+	    out = new DataOutputStream(connectionSocket.getOutputStream());
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
 
-
+    @Override
+    public void run() {
+	System.out.println("hi");
+    }
 
 }
